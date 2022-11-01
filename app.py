9cd05e71095215de508317d1597e9c30eda45fd2
amir <amir@amirnoshian1@gmail.com>
2022-11-01 15:06:37 +0330

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from flask import Flask
+from flask import jsonify
+import socket
+import psutil
+app = Flask(__name__)
+@app.route('/')
+def default():
+    return "go to status page"
+
+
+@app.route('/status')
+def name():
+    hostname = socket.gethostname()
+    Ipaddres = socket.gethostbyname(hostname)
+    date = datetime.now()
+    cpu = psutil.cpu_percent(interval=0.5)
+
+    mem_value = (psutil.swap_memory())
+
+
+    result = {
+
+            "status system": "up",
+            "hostname": hostname,
+            "ipaddres": Ipaddres,
+            "date": date,
+            "mem": mem_value,
+            "cpu_usage is ": cpu,
+            }
+
+    return jsonify(str(result))
